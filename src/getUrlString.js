c674b7a9495c9769fe01ecc7b561da85b1ccb405
William Brendel <brendel.william@gmail.com>
2024-05-15 16:40:52 -0700

    index

--- src/getUrlString.js
+++ src/getUrlString.js
@@ -3,7 +3,7 @@
 // Utility function to get a url string.
 export const getUrlString = url => {
   typeof url === 'string' || (url && (url = `${url}`)) || (url = '');
-  return url.toLowerCase().replace(/s+/g, '');
+  return url.toLowerCase().replace(/\s+/g, '');
 }
 
 // Exports.
