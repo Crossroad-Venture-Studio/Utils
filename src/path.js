f9320cc4c7074a41613a8342da4e5912a3397275
William Brendel <brendel.william@gmail.com>
2024-06-06 15:39:42 -0700

    index

--- src/path.js
+++ src/path.js
@@ -7,11 +7,19 @@ export const urlbasename = Path.urlbasename = url => {
   } catch {}
 
   url = url.replace(/^(https|http|ftp)\:\/\//i, '');
-  url = url.slice(0, url.indexOf('/'));
-  return url.slice(0, url.lastIndexOf('.')).trim();
+  url = url.slice(0, Math.max(url.indexOf('/') || 0, 0));
+  return url.slice(0, Math.max(url.lastIndexOf('.') || 0, 0)).trim();
 }
 
 export const urlname = Path.urlname = url => urlbasename(url).split('.').reverse().join(' ');
+
+export const urlpage = Path.urlpage = url => {
+  url || (url = '');
+  try {
+    if (url instanceof URL) url = url.hostname;
+  } catch {}
+  url.slice(Math.max(url.indexOf('/'), 0), Math.min(Math.max(url.indexOf('?') || 0, 0), Math.max(url.indexOf('#') || 0, 0), url.length));
+};
 
 // Deafult export.
 export default Object.freeze(Path);
